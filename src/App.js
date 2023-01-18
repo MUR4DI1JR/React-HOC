11fa4ef3dcf9bb92a1f7d4d58ecaabdb40bf99c8
muradil <04072002mu@gmail.com>
2023-01-18 19:01:23 +0600

    HOC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {createContext, useState} from 'react';
+import ButtonUI from "./utils/ButtonUI";
+import {composeComponent} from "./hoc/composeComponent";
+import CatComponent from "./components/catComponent";
+import MyComponent from "./components/MyComponent";
+import {WithLoading} from "./hoc/withLoading";
+import TypingText from "./utils/typingText";
+import Loader from "./hoc/loader";
+import windowBrowser from "./hoc/windowBrowser";
+import windowView from "./components/windowView";
+import ColorChange from "./hoc/colorChange";
+import ItemComponent from "./components/itemComponent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    // const ComposedCatComponent = composeComponent(CatComponent);
+    //
+    // const HelloWorldComponent = composeComponent(MyComponent);
+    //
+    // const IsLoadingComponent = WithLoading(MyComponent);
+    // const IsLoadingCatComponent = WithLoading(CatComponent);
+
+    const LoadingText = Loader(MyComponent, 3000);
+    const LoadingCatComponent = Loader(CatComponent, 6000);
+
+    const MyComponentWithWindowSize = windowBrowser(windowView);
+
+    const ColorChangeComponent = ColorChange(ItemComponent, "dark");
+
+    return (
+        <div>
+            <LoadingText/>
+            <LoadingCatComponent/>
+            <MyComponentWithWindowSize/>
+            <ColorChangeComponent/>
+            {/*<ComposedCatComponent/>*/}
+            {/*<HelloWorldComponent/>*/}
+            {/*<IsLoadingComponent/>*/}
+            {/*<IsLoadingCatComponent/>*/}
+        </div>
+    );
+};
 
 export default App;
